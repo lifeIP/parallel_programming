edf6470d7916b0788da5f77e961b089379e15589
lifeIP <97748864+lifeIP@users.noreply.github.com>
2022-03-10 16:56:39 +0700

    2

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <thread>
+int s = 0;
 
 void hello() {
-	std::cout << "Hello world!!!";
+	std::cout << "Hello world!!!\n";
+	s++;
 }
 
 int main() {
 	std::thread t(hello);
 	t.join();
+	std::cout << s;
 }
